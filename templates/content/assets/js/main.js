53858ecc0c8b8bc7ec46c3c7b18371e2c65140db
tkmfujise <106900036+tkmfujise@users.noreply.github.com>
2025-12-18 00:35:31 +0900

    Update templates

--- templates/content/assets/js/main.js
+++ templates/content/assets/js/main.js
@@ -2,18 +2,29 @@ function toggleContentsMenu() {
   const elem = document.querySelector('aside.contents-menu')
   if (!elem) return;
 
-  if (!elem.style.visibility || elem.style.visibility === 'hidden') {
-    elem.style.visibility = 'visible'
+  if (elem.classList.contains('open')) {
+    elem.classList.remove('open')
   }
   else {
-    elem.style.visibility = 'hidden'
+    elem.classList.add('open')
   }
 }
 
+
+function hideContentsMenu() {
+  const elem = document.querySelector('aside.contents-menu')
+  if (!elem) return;
+  elem.classList.remove('open')
+}
 
 
 window.onload = () => {
   document.querySelectorAll('.toggle-contents-menu').forEach(elem => {
     elem.addEventListener('click', toggleContentsMenu)    
   })
+
+  const article = document.querySelector('article.content')
+  if (article) {
+    article.addEventListener('click', hideContentsMenu)
+  }
 }
