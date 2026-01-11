8b69d0146aa9a19f55fc3adf6531c5dd514fcaa3
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-01-11 18:29:41 +0900

    Add .gdignore to ignoring paths

--- lib/gd_doc/collector.rb
+++ lib/gd_doc/collector.rb
@@ -11,17 +11,24 @@ module GdDoc
         files.map{|file| new(file) }
       end
 
+      # Ignoring: GdDoc.config.igoring_paths and directories that contain .gdignore
       def files
         targets = extensions.map{|ext| "#{GdDoc.config.project_dir_absolute}/**/*.#{ext}" }
+        gdignores = gdignore_directories
         Dir[*targets].reject{|path|
-            next true if File.directory?(path)
-            rel_path = relativized_path(path)
-            GdDoc.config.ignoring_paths.any?{|str| rel_path.to_s.start_with? str }
-          }
+          next true if File.directory?(path)
+          next true if gdignores.any?{|dir| path.start_with?(dir)}
+          rel_path = relativized_path(path)
+          GdDoc.config.ignoring_paths.any?{|str| rel_path.to_s.start_with? str }
+        }
       end
 
       def relativized_path(path)
         Pathname(path).realpath.relative_path_from(GdDoc.config.project_dir_absolute)
+      end
+
+      def gdignore_directories
+        Dir["#{GdDoc.config.project_dir_absolute}/**/.gdignore"].map{|d| d.delete_suffix('.gdignore') }
       end
     end
 
